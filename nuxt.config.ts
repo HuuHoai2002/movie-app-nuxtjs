86bd1d6f105ee740aa2b3b0c4760d041a4f37a36
Huuhoai2002 <huuhoai0511@gmail.com>
2022-09-22 22:18:47 +0700

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: ['@/assets/css/main.css'],
-  generate: { fallback: '404.html' },
+  // generate: { fallback: '404.html', exclude: [/^\/admin/] },
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: ['~plugins/axios.ts', '~plugins/repository.ts'],
